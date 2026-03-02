002adb1eb76ad14555c7d618ed390915d12ddee3
jeffkemp25686-hash <jeffkemp25686@gmail.com>
2026-03-03 10:29:21 +1100

    Wire bootApp into Vite entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { bootApp } from "./appCore.js";
 
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
 );
+bootApp();
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
